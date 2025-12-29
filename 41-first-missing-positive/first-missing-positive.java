5351a8575a34af165b97f211fc433b7997c4f742
LOGITH J <110965723+LOGITH2003@users.noreply.github.com>
2025-12-29 23:13:20 +0530

    Time: 1 ms (99.80%) | Memory: 71.2 MB (67.89%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,6 +1,9 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        for(int i = 0;i<nums.length;i++){
+        for(int i = 0;i<nums.length;i++){            
+            //    Case 1: It will contain duplicate number you need to manage.
+            //    Case 2: It will contain negative number also you manage.
+            //    Case 3: It will contain high number compare to array length you need to manage.
             if(nums[i]< nums.length && nums[i] > 0 && nums[nums[i]-1] != nums[i] && nums[i] != i +1){
                 swap(nums, nums[i]-1, i);
                 i--;
